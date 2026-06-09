b6669674ab83293d826f574ab9e48187dd706438
Igor Bione <39741256+igorbione@users.noreply.github.com>
2026-06-09 16:25:31 -0300

    Change base path in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 
 export default defineConfig({
-  base: "/ecoly-data-craft/",   // <-- add this
+  base: "/igor-bione-portfolio/",   // <-- add this
   tanstackStart: {
     server: { entry: "server" },
   },
